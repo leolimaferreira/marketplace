6ff8ca1e6dfb1b25b171fea44e435e1a1989fdc9
Leonardo Lima <leo2342005@gmail.com>
2025-11-28 14:18:54 -0300

    feat: adicionando validacao para que o pedidoId nao seja nulo na criacao de pagamentos.

--- src/main/java/com/marketplace/dto/pagamento/PagamentoCriacaoDTO.java
+++ src/main/java/com/marketplace/dto/pagamento/PagamentoCriacaoDTO.java
@@ -1,12 +1,14 @@
 package com.marketplace.dto.pagamento;
 
 import com.marketplace.annotation.FormaPagamentoValida;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
 public record PagamentoCriacaoDTO(
         @FormaPagamentoValida
         String formaPagamento,
+        @NotNull(message = "Id do produto é obrigatório")
         UUID pedidoId
 ) {
 }
