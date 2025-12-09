65b21b48b7189ad1a66875d8854e6984107a6890
Leonardo Lima <leo2342005@gmail.com>
2025-12-09 10:41:25 -0300

    feat: adicionando endpoint para atualizacao de avaliacoes, ja com validacoes.

--- src/main/java/com/marketplace/dto/avaliacao/AvaliacaoAtualizacaoDTO.java
+++ src/main/java/com/marketplace/dto/avaliacao/AvaliacaoAtualizacaoDTO.java
@@ -2,16 +2,11 @@ package com.marketplace.dto.avaliacao;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.List;
-import java.util.UUID;
 
-public record AvaliacaoCriacaoDTO(
-        @NotNull(message = "O ID do pedido é obrigatório")
-        UUID pedidoId,
-        @NotNull(message = "A nota da avaliação é obrigatória")
+public record AvaliacaoAtualizacaoDTO(
         @Min(value = 1, message = "A nota da avaliação deve ser no mínimo 1")
         @Max(value = 5, message = "A nota da avaliação deve ser no máximo 5")
         Integer nota,
